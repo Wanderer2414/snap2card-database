ea87b5b61b75a01b54e2a152fc8a937cc1812797
Wanderer2414 <huynguyenlequoc13@gmail.com>
2026-08-30 21:21:07 +0700

    feat: account logout

---
+++ schema/procedures/update/account_logout.sql
@@ -0,0 +1,19 @@
+-- CREATE    TABLE SESSION (
+--           session_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY    ,
+--           login_time TIMESTAMPTZ DEFAULT NOW() NOT NULL              ,
+--           logout_time TIMESTAMPTZ DEFAULT (NOW() + INTERVAL '15m')   ,
+--           final_status TYPE_STATUS_SESSION NOT NULL DEFAULT 'ACTIVE' ,
+--           owner_id INT NOT NULL                                      ,
+--           FOREIGN KEY (owner_id) REFERENCES ACCOUNT (account_id)
+--           );
+CREATE
+OR        REPLACE PROCEDURE PR_ACCOUNT_LOGOUT (p_id INT) AS $$ BEGIN
+    UPDATE SESSION
+    SET final_status = 'ENDED', logout_time = NOW()
+    WHERE ownder_id = p_id AND final_status = 'ACTIVE';
+
+    IF NOT FOUND THEN
+        RAISE EXCEPTION 'Unauthorized';
+    END IF;
+END
+$$ LANGUAGE plpgsql;

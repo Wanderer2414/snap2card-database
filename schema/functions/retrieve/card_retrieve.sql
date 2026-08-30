3b379b6e2270a810bef2c7ca1256521476bd5f4b
Wanderer2414 <huynguyenlequoc13@gmail.com>
2026-08-30 23:18:31 +0700

    feat: card_retrieve

---
+++ schema/functions/retrieve/card_retrieve.sql
@@ -0,0 +1,29 @@
+CREATE
+OR        REPLACE FUNCTION FN_CARD_RETRIEVE (p_ids INT[]) RETURNS TABLE (
+          card_id INT        ,
+          frontside_text TEXT,
+          backside_text TEXT
+          ) AS $$
+BEGIN
+    RETURN QUERY 
+    SELECT C.card_id, FS.component_text, BS.component_text FROM CARD C
+    JOIN COMPONENT FS ON FS.component_id = C.frontside_id
+    JOIN COMPONENT BS ON BS.component_id = C.backside_id
+    WHERE C.card_id in (SELECT unnest(p_ids));
+END
+$$ LANGUAGE plpgsql;
+
+CREATE
+OR        REPLACE FUNCTION FN_CARD_RETRIEVE (p_id INT) RETURNS TABLE (frontside_text TEXT, backside_text TEXT) AS $$
+BEGIN
+    RETURN QUERY 
+    SELECT FS.component_text, BS.component_text FROM CARD C
+    JOIN COMPONENT FS ON FS.component_id = C.frontside_id
+    JOIN COMPONENT BS ON BS.component_id = C.backside_id
+    WHERE C.card_id = p_id;
+END
+$$ LANGUAGE plpgsql;
+
+-- DROP      FUNCTION FN_CARD_RETRIEVE;
+SELECT    *
+FROM      FN_CARD_RETRIEVE (3)

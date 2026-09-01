d3973566ded09209f231dc64095bf235e77edfe1
Wanderer2414 <huynguyenlequoc13@gmail.com>
2026-09-01 23:01:38 +0700

    fix: add name for array in category retrieve

--- protocols/category_retrieve.sql
+++ protocols/category_retrieve.sql
@@ -14,7 +14,7 @@ OR        REPLACE FUNCTION CATEGORY_RETRIEVE (p_session_id TYPE_ID, p_id TYPE_ID
           BEGIN
             RETURN QUERY 
             SELECT C.category_name, C.numOfCard, 
-                  (SELECT array_agg(FN_CARD_ID(CID.card_id)) FROM unnest(C.card_ids) as CID(card_id)), 
+                  (SELECT array_agg(FN_CARD_ID(CID.card_id)) FROM unnest(C.card_ids) as CID(card_id)) AS card_ids, 
                   C.YEAR, C.MONTH, C.DAY, C.HOUR, C.MINUTE, C.SECOND, C.gmt 
             FROM FN_CATEGORY_RETRIEVE(FN_SESSION_CHECK(p_session_id), FN_ID_CATEGORY(p_id)) C;
           END

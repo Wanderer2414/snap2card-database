b01b23f4b24c418f4a5d369a7d63ec63e03227ca
Wanderer2414 <huynguyenlequoc13@gmail.com>
2026-09-01 21:15:14 +0700

    fix: card_list return wrong type

--- protocols/card_list.sql
+++ protocols/card_list.sql
@@ -1,8 +1,8 @@
 CREATE
-OR        REPLACE FUNCTION CARD_LIST (p_session TYPE_ID) RETURNS TABLE (card_id INT, component_text TEXT) AS $$
+OR        REPLACE FUNCTION CARD_LIST (p_session TYPE_ID) RETURNS TABLE (card_id TYPE_ID, component_text TEXT) AS $$
 BEGIN
     RETURN QUERY
-    SELECT A.card_id, C.component_text FROM ACCOUNT_CARD_HAVE A 
+    SELECT FN_CARD_ID(A.card_id), C.component_text FROM ACCOUNT_CARD_HAVE A 
     JOIN CARD CD ON A.card_id = CD.card_id
     JOIN COMPONENT C ON CD.frontside_id = C.component_id
     WHERE A.account_id = FN_SESSION_CHECK(p_session);
